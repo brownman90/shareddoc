bf096cb8308f8957574933d52b0360ac8fa79b8b
chenzhouce <chenzhouce@gmail.com>
2014-02-09 16:51:28 +0800

    modify create and delete dir

--- src/main/java/com/zchen/sdp/controller/DirController.java
+++ src/main/java/com/zchen/sdp/controller/DirController.java
@@ -1,15 +1,20 @@
 package com.zchen.sdp.controller;
 
 import com.zchen.extjsassistance.base.model.AjaxResult;
+import com.zchen.extjsassistance.fs.DirectoryConfig;
+import com.zchen.extjsassistance.fs.DirectoryNodeFactory;
+import com.zchen.extjsassistance.fs.ExtjsDirectoryAssistant;
+import com.zchen.extjsassistance.fs.ExtjsFileFilter;
 import com.zchen.extjsassistance.fs.model.DirectoryNode;
-import com.zchen.sdp.bean.Dir;
 import com.zchen.sdp.service.DirService;
-import org.apache.commons.io.FileExistsException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.FileAlreadyExistsException;
 
 /**
  * @author Zhouce Chen
@@ -19,6 +24,7 @@ import javax.annotation.Resource;
 @RequestMapping("/dir")
 public class DirController {
 
+    String rootAbsolutePath = "D:/shared_doc";
     @Resource
     private DirService dirService;
 
@@ -26,19 +32,33 @@ public class DirController {
     public
     @ResponseBody
     DirectoryNode getDirectoryTree(String node) {
-        return dirService.getDirectoryTree(node);
+        ExtjsFileFilter fileFilter = new ExtjsFileFilter();
+        fileFilter.setIgnoreFile(true);
+        fileFilter.setIgnoreHidden(true);
+
+
+        DirectoryConfig config = new DirectoryConfig();
+        config.setFileFilter(fileFilter);
+        config.setRootPath(rootAbsolutePath);
+        config.setLevel(0);
+
+        DirectoryNode top = DirectoryNodeFactory.build().newTopNode(node, rootAbsolutePath);
+        return ExtjsDirectoryAssistant.getFileSystemTree(top, config);
     }
 
 
     @RequestMapping("/delete")
     public
     @ResponseBody
-    AjaxResult delete(Dir dir) {
+    AjaxResult delete(DirectoryNode directoryNode) {
         try {
-            dirService.delete(dir);
-        } catch (FileExistsException e) {
+            String path = rootAbsolutePath + directoryNode.getId();
+            ExtjsDirectoryAssistant.deleteDirectory(path);
+        } catch (DirectoryNotEmptyException e) {
             return AjaxResult.get().failure()
-                    .setMessage("Delete directory failed. " + e.getMessage());
+                    .setMessage("Delete directory failed. Directory " + directoryNode.getId() + " is not empty.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return AjaxResult.get().success();
     }
@@ -46,12 +66,15 @@ public class DirController {
     @RequestMapping("/create")
     public
     @ResponseBody
-    AjaxResult create(Dir dir) {
+    AjaxResult create(DirectoryNode directoryNode) {
         try {
-            dirService.create(dir);
-        } catch (FileExistsException e) {
+            String path = rootAbsolutePath + directoryNode.getId();
+            ExtjsDirectoryAssistant.createDirectory(path);
+        } catch (FileAlreadyExistsException e) {
             return AjaxResult.get().failure()
-                    .setMessage("Create directory failed. " + e.getMessage());
+                    .setMessage("Create directory failed. Directory " + directoryNode.getId() + " has exists.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return AjaxResult.get().success();
     }
--- src/main/java/com/zchen/sdp/service/impl/DirServiceImpl.java
+++ src/main/java/com/zchen/sdp/service/impl/DirServiceImpl.java
@@ -1,78 +1,15 @@
 package com.zchen.sdp.service.impl;
 
-import com.zchen.extjsassistance.fs.DirectoryConfig;
-import com.zchen.extjsassistance.fs.ExtjsDirectoryAssistant;
-import com.zchen.extjsassistance.fs.DirectoryNodeFactory;
-import com.zchen.extjsassistance.fs.ExtjsFileFilter;
-import com.zchen.extjsassistance.fs.model.DirectoryNode;
-import com.zchen.sdp.bean.Dir;
 import com.zchen.sdp.service.DirService;
-import org.apache.commons.io.FileExistsException;
 import org.springframework.stereotype.Service;
-
-import java.io.File;
 
 /**
  * Implementation of SDP document storage directories related operation
+ *
  * @author Zhouce Chen
  * @version Jan 18, 2014
  */
 @Service
 public class DirServiceImpl implements DirService {
-    String rootAbsolutePath = "D:/shared_doc";
 
-    /**
-     * Get the directory tree of SDP storage location
-     * @param node  The top directory node path
-     * @return  The top directory node
-     */
-    @Override
-    public DirectoryNode getDirectoryTree(String node) {
-        String rootPath = "D:/shared_doc";
-        ExtjsFileFilter fileFilter = new ExtjsFileFilter();
-        fileFilter.setIgnoreFile(true);
-        fileFilter.setIgnoreHidden(true);
-
-
-        DirectoryConfig config = new DirectoryConfig();
-        config.setFileFilter(fileFilter);
-        config.setRootPath(rootPath);
-        config.setLevel(0);
-
-        DirectoryNode top = DirectoryNodeFactory.build().newTopNode(node, rootPath);
-        return ExtjsDirectoryAssistant.getFileSystemTree(top, config);
-    }
-
-    /**
-     * Create new directory
-     * @param dir  The directory to create
-     * @throws FileExistsException
-     */
-    @Override
-    public void create(Dir dir) throws FileExistsException {
-        String path = rootAbsolutePath + dir.getId();
-        File file = new File(path);
-        if (file.exists()) {
-            throw new FileExistsException("The directory exists.");
-        }
-        boolean result = file.mkdir();
-        if (!result) {
-
-        }
-    }
-
-    /**
-     * Delete directory
-     * @param dir The directory to delete
-     * @throws FileExistsException
-     */
-    @Override
-    public void delete(Dir dir) throws FileExistsException {
-        String path = rootAbsolutePath + dir.getId();
-        File file = new File(path);
-        boolean result = file.delete();
-        if (!result) {
-            throw new FileExistsException("The directory is not empty.");
-        }
-    }
 }
